7a9d0eaf11f7204083d2d4856b8fdc6e4127a840
Connor Davis <mail@connordav.is>
2018-10-17 18:13:38 -0500

    Add Cypress Tests

---
+++ cypress/integration/home_page.spec.js
@@ -0,0 +1,72 @@
+describe('The Home Page', function () {
+  it('can navigate to Origin', function () {
+    cy.visit('/')
+    cy.contains('Origin').click()
+    cy.url().should('include', '/origin')
+
+    cy.visit('/open-source')
+    cy.contains('Origin').click()
+    cy.url().should('include', '/origin')
+
+    cy.visit('/art')
+    cy.contains('Origin').click()
+    cy.url().should('include', '/origin')
+  })
+
+  it('can navigate to Open Source', function () {
+    cy.visit('/')
+    cy.contains('Open Source').click()
+    cy.url().should('include', '/open-source')
+
+    cy.visit('/art')
+    cy.contains('Open Source').click()
+    cy.url().should('include', '/open-source')
+
+    cy.visit('/origin')
+    cy.contains('Open Source').click()
+    cy.url().should('include', '/open-source')
+  })
+
+  it('can navigate to Art', function () {
+    cy.visit('/')
+    cy.contains('Art').click()
+    cy.url().should('include', '/art')
+
+    cy.visit('/open-source')
+    cy.contains('Art').click()
+    cy.url().should('include', '/art')
+
+    cy.visit('/origin')
+    cy.contains('Art').click()
+    cy.url().should('include', '/art')
+  })
+
+  it('can navigate back to Projects', function () {
+    cy.visit('/art')
+    cy.contains('Projects').click()
+    cy.url().should('include', '/projects')
+
+    cy.visit('/open-source')
+    cy.contains('Projects').click()
+    cy.url().should('include', '/projects')
+
+    cy.visit('/origin')
+    cy.contains('Projects').click()
+    cy.url().should('include', '/projects')
+  })
+
+  it('can click on each Project', function () {
+    cy.visit('/')
+    cy.get(':nth-child(1) > :nth-child(2) > div > h3').click()
+    cy.url().should('include', '/projects/authenticity-platform')
+  })
+
+  it('can load each page', function () {
+    cy.visit('/')
+    cy.visit('/origin')
+    cy.visit('/open-source')
+    cy.visit('/art')
+    cy.visit('/projects')
+    cy.visit('/projects/authenticity-platform')
+  })
+})
--- subsections/OpenSource.js
+++ subsections/OpenSource.js
@@ -1,11 +1,9 @@
 // @flow
 import * as React from 'react'
 import Cards from '../components/Cards'
-import CondensedCard from '../components/CondensedCard'
-import { header as AuthenticyPlatform } from '../pages/projects/authenticity-platform'
 import { backgrounds } from '../helpers/constants'
 
 export const background = backgrounds.green.key
 export default () => <Cards background={background}>
-  <CondensedCard header={AuthenticyPlatform} location='/projects/authenticity-platform' />
+  <div>Coming soon...</div>
 </Cards>
