d870bae4426db6bc1749b88412f4cf9862d305d5
dav-is <mail@connordav.is>
2022-01-30 18:25:26 -0500

    Use Next.js headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  async headers () {
+    return [
+      { source: '/:path*', headers: [{ key: 'access-control-allow-origin', value: 'https://app.element.io' }] },
+      { source: '/:path*', headers: [{ key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload;' }] }
+    ]
+  },
   async rewrites () {
     return {
       beforeFiles: [
