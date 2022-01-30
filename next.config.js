770745da03dbaed952e07959fd04d036e3895c1e
dav-is <mail@connordav.is>
2022-01-30 18:29:44 -0500

    Don't rewrite to matrix

--- next.config.js
+++ next.config.js
@@ -4,19 +4,5 @@ module.exports = {
       { source: '/:path*', headers: [{ key: 'access-control-allow-origin', value: 'https://app.element.io' }] },
       { source: '/:path*', headers: [{ key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload;' }] }
     ]
-  },
-  async rewrites () {
-    return {
-      beforeFiles: [
-        {
-          source: '/_matrix',
-          destination: 'https://matrix.connordav.is/_matrix'
-        },
-        {
-          source: '/_matrix/:path*',
-          destination: 'https://matrix.connordav.is/_matrix/:path*'
-        }
-      ]
-    }
   }
 }
