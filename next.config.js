710a1d42bd36ac829f204bf0f6ae0ddb2f662563
dav-is <mail@connordav.is>
2022-02-03 17:21:04 -0500

    Fix CORS

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   async headers () {
     return [
-      { source: '/\.well-known/matrix/:path*', headers: [{ key: 'access-control-allow-origin', value: 'https://matrix.connordav.is' }] },
+      { source: '/\.well-known/matrix/:path*', headers: [{ key: 'access-control-allow-origin', value: '*' }] },
       { source: '/:path*', headers: [{ key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload;' }] }
     ]
   }
