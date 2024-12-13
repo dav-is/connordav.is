6f0b4c91f81556aa2c3bba82058a375f5f050be1
dav-is <mail@connordav.is>
2024-12-13 18:02:48 -0500

    Add speed insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
+      <SpeedInsights />
       <Analytics />
     </>
   )
